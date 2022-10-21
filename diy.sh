b049eaa079071081ae05a4f8f0bb1571c72c1cd1
zhaoxh5 <67985916+zhaoxh5@users.noreply.github.com>
2022-10-22 01:03:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -19,12 +19,8 @@ sed -i 's/OpenWrt/Phicomm_N1/g' ./package/base-files/files/bin/config_generate
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 #git clone https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash  #openclash出国软件
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-amlogic package/lean/luci-app-openclash  #openclash出国软件
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash  #openclash出国软件
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/lean/package  #passwall基本依赖
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall  #passwall出国软件
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic  #晶晨宝盒
 echo "src-git ssrp https://github.com/fw876/helloworld.git" >> ./feeds.conf.default  #添加SSR-Plus
-
-#rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
-#全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
-#用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
