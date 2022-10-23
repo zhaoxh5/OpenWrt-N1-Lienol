4b693306a0bbc7f381d35e3eceba87fae93b15c4
zhaoxh5 <67985916+zhaoxh5@users.noreply.github.com>
2022-10-23 22:48:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -21,5 +21,5 @@ sed -i 's/OpenWrt/Phicomm_N1/g' ./package/base-files/files/bin/config_generate
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/lean/package  #passwall基本依赖
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall  #passwall出国软件
 #svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash  #openclash出国软件
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic  #晶晨宝盒
+#svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic  #晶晨宝盒
 #echo "src-git ssrp https://github.com/fw876/helloworld.git" >> ./feeds.conf.default  #添加SSR-Plus
